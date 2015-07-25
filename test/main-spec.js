d4c70f42d6d04ff9f6094be69ce9d0e7a3b70c2a
Nils Knappmeier <n.knappmeier@i-views.de>
2015-07-26 01:01:05 +0200

    More (now failing) testcases

--- test/main-spec.js
+++ test/main-spec.js
@@ -12,22 +12,76 @@
 
 'use strict'
 
-var promisedHandlebars = require('../')
 var Q = require('q')
-var expect = require('chai').expect
+
+// Chai Setup
+var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+var expect = chai.expect;
+
+// Handlebars-Setup
+var promisedHandlebars = require('../')
+var Handlebars = promisedHandlebars(require('handlebars'))
+
+Handlebars.registerHelper({
+  'helper': function (delay, value) {
+    return Q.delay(delay).then(function () {
+      return 'h(' + value + ')'
+    })
+  },
+  'block': function (delay, value, options) {
+    return Q.delay(delay).then(function () {
+      return 'b(' + options.fn(value) + ')'
+    })
+  }
+})
+
+Handlebars.registerPartial('a',"{{helper '10' 'partialA'}}")
+Handlebars.registerPartial('b',"{{helper '10' 'partialB'}}")
 
 describe('promised-handlebars:', function () {
-  it('should return a promise for the ouput with helpers resolved', function (done) {
-    var Handlebars = promisedHandlebars(require('handlebars'))
-    Handlebars.registerHelper('p', function (value) {
-      return Q.delay(100).then(function () {
-        return value
-      })
-    })
-    Handlebars.compile('123{{p a}}456{{p b}}')({ a: 'abc', b: 'xyz'}).then(
-      function ( result) {
-        expect(result).to.equal('123abc456xyz')
-      }).done(done)
+  xit('should return a promise for the ouput with helpers resolved', function (done) {
+    var template = Handlebars.compile(fixture('simple-helper.hbs'))
+    return expect(template({a: 'abc', b: 'xyz'}))
+      .to.eventually.equal('123 h(abc) 456 h(xyz)')
+      .notify(done);
   })
 
+  xit('should work with block helpers that call `fn` while resolving a promise', function (done) {
+    var template = Handlebars.compile(fixture('block-helper.hbs'))
+    return expect(template({a: 'abc', b: 'xyz'}))
+      .to.eventually.equal('123 b(abc) 456')
+      .notify(done);
+  })
+
+  xit('should work with a helper being called from within a block helpers', function (done) {
+    var template = Handlebars.compile(fixture('nested-helpers.hbs'))
+    return expect(template({a: 'abc', b: 'xyz'}))
+      .to.eventually.equal('123 b(h(abc)) 456')
+      .notify(done);
+  })
+
+  xit('should handle {{expr}} and {{{expr}}} like Handlebars does', function (done) {
+    var template = Handlebars.compile(fixture('escaping.hbs'))
+    return expect(template({a: '<a>' , b: '<b>'}))
+      .to.eventually.equal('raw: <a> h(<a>) esc: &lt;a&gt; h(&lt;a&gt;)')
+      .notify(done);
+
+  })
+
+  it('should work correctly when partials are called', function (done) {
+    var template = Handlebars.compile(fixture('partials.hbs'))
+    return expect(template({a: 'aa' , b: 'bb'}))
+      .to.eventually.equal('h(partialA) 123 h(aa) h(partialB) 456 h(bb)')
+      .notify(done);
+  })
 })
+
+
+function fixture (file) {
+  var fs = require('fs')
+  return fs.readFileSync(require.resolve('./fixtures/' + file), {encoding: 'utf-8'}).trim()
+}
+
+;
