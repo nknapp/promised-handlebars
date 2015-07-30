7686c2968d8ed90d0a278e430501f553923914a6
Nils Knappmeier <n.knappmeier@i-views.de>
2015-07-31 00:17:05 +0200

    Cleaner code, hopefully more readable

--- index.js
+++ index.js
@@ -38,69 +38,32 @@ module.exports = function promisedHandlebars (Handlebars, options) {
   var engine = Handlebars.create()
 
   // Wrap `registerHelper` with a custom function
-  var oldRegisterHelper = engine.registerHelper
-  engine.registerHelper = function (keyOrObject, fn) {
+  engine.registerHelper = wrap(engine.registerHelper, function (fn, args) {
+    var keyOrObject = args[0]
+    var value = args[1]
+    // Wrap helpers provided in arguments
     if (typeof keyOrObject === 'string') {
-      // Register a custom helper-function instead of actual helper
-      oldRegisterHelper.call(this, keyOrObject, function () {
-        // Wrap "optons.fn" and "options.inverse" to apply the same logic as for the template itself
-        var helperOpts = arguments[arguments.length - 1]
-        if (helperOpts.fn) {
-          helperOpts.fn = wrapAndResolve(helperOpts.fn)
-        }
-        if (helperOpts.inverse) {
-          helperOpts.inverse = wrapAndResolve(helperOpts.inverse)
-        }
-
-        // If the result is not promise, return it
-        var rawResult = fn.apply(this, arguments)
-        if (!Q.isPromiseAlike(rawResult)) {
-          return rawResult
-        }
-
-        // Check if the promise is already resolved
-        var immediateResult
-        var immediateError
-        var result = Q(rawResult)
-          .then(function (result) {
-            // Fetch result if it is available in the same event-loop cycle
-            // Store an object to prevent the result being falsy
-            immediateResult = {result: result}
-            return result
-          })
-          .catch(function (error) {
-            // Fetch error if it is available in the same event-loop cycle
-            // Store an object to prevent the result being falsy
-            immediateError = {error: error}
-          })
-
-        if (immediateResult) {
-          return immediateResult.result
-        }
-        if (immediateError) {
-          return immediateError.error
-        }
-
-        promises.push(result)
-        return options.placeholder + (promises.length - 1) + '>'
-
-      })
+      // Wrap single helper
+      fn.call(this, keyOrObject, wrap(value, helper))
     } else {
-      // `keyOrObject` is actual an object of helpers
-      // Call `registerHelper` again for each object
-      // This simulates the default Handlebars-behaviour
-      Object.keys(keyOrObject).forEach(function (key) {
-        engine.registerHelper(key, keyOrObject[key])
-      })
+      // Key is an object. Wrap all helpers
+      for (var key in keyOrObject) {
+        var wrappedHelpers = {}
+        if (keyOrObject.hasOwnProperty(key)) {
+          wrappedHelpers[key] = wrap(keyOrObject[key], helper)
+        }
+        fn.call(this, wrappedHelpers)
+      }
     }
-  }
+  })
 
-  var oldCompile = engine.compile
-  engine.compile = function () {
-    var fn = oldCompile.apply(this, arguments)
-    return wrapAndResolve(fn)
+  // Wrap the `compile` function, so that it wraps the compiled-template
+  // with `prepareAndResolve`
+  engine.compile = wrap(engine.compile, function (oldCompile, args) {
+    var fn = oldCompile.apply(engine, args)
+    return wrap(fn, prepareAndResolve)
   // Wrap the compiled function
-  }
+  })
 
   // Re-register all built-in-helpers to ensure that their methods are wrapped
   engine.registerHelper(engine.helpers)
@@ -113,39 +76,115 @@ module.exports = function promisedHandlebars (Handlebars, options) {
    * 3) promise placeholder-values are replaced with the promise-results
    *    in the returned promise
    */
-  function wrapAndResolve (fn) {
-    return function () {
-      if (promises) {
-        // "promises" array already exists: We are executing a partial
-        // or a synchronous block helper.
-        // That means we are called from somewhere within Handlebars.
-        // Handlebars does not like promises, so we act as normal as possible.
-        return fn.apply(undefined, arguments)
-      }
-      try {
-        // We are called from outside Handlebars (initial call)
-        // Initialize the `promise` variable (see above) with an empty
-        // array, so that helpers can store their promises during the
-        // template execution.
-        promises = []
-        // Execute template (helpers are getting called and store promises
-        // into the array
-        var resultWithPlaceholders = fn.apply(undefined, arguments)
-        return Q.all(promises).then(function (results) {
-          // Promises are fulfilled. Insert real values into the result.
-          return String(resultWithPlaceholders).replace(regex, function (match, index, gt) {
-            // Check whether promise result must be escaped
-            return gt === '>' ? results[index] : engine.escapeExpression(results[index])
-          })
+  function prepareAndResolve (fn, args) {
+    if (promises) {
+      // "promises" array already exists: We are executing a partial or a synchronous block
+      // helper. That means we are called from somewhere within Handlebars. Handlebars does
+      // not like promises, so we act as normal as possible.
+      return fn.apply(this, args)
+    }
+    try {
+      // We are called from outside Handlebars (initial call). Initialize the `promise`
+      // variable (see above) with an empty array, so that helpers can store their promises
+      // during the template execution.
+      promises = []
+      // Execute template (helpers are getting called and store promises into the array
+      var resultWithPlaceholders = fn.apply(this, args)
+      return Q.all(promises).then(function (results) {
+        // Promises are fulfilled. Insert real values into the result.
+        return String(resultWithPlaceholders).replace(regex, function (match, index, gt) {
+          // Check whether promise result must be escaped
+          return gt === '>' ? results[index] : engine.escapeExpression(results[index])
         })
-      } finally {
-        // Reset promises for the next execution run
-        promises = null
-      }
+      })
+    } finally {
+      // Reset promises for the next execution run
+      promises = null
+    }
+  }
 
+  /**
+   * Wrapper function for a helper.
+   * Wrap `options.fn` and `options.inverse` to
+   * return promises (eventually)
+   * and call `markAndPromise` afterwards
+   */
+  function helper (fn, args) {
+    var _this = this
+    var options = args[args.length - 1]
+    if (options.fn) {
+      options.fn = wrap(options.fn, prepareAndResolve)
+    }
+    if (options.inverse) {
+      options.inverse = wrap(options.inverse, prepareAndResolve)
+    }
+    return markAndPromise.call(_this, fn, args)
+  }
+
+  /**
+   * Apply a function with arguments.
+   * If a promise is returned by the function, push it into
+   * the promises-array and set its `.toString` method
+   * to
+   * * the placeholder, followed by
+   * * the index in thesarray
+   * * ">"
+   *
+   * @param fn the original function
+   * @param arguments the arguments passed to the original function
+   * @returns {*}
+   */
+  function markAndPromise (fn, args) {
+    var rawResult = fn.apply(this, args)
+
+    if (!Q.isPromiseAlike(rawResult)) {
+      return rawResult
     }
 
+    // Check if the promise is already resolved
+    var immediateResult = null
+    var immediateError = null
+    var result = Q(rawResult)
+      .then(function (result) {
+        // Fetch result if it is available in the same event-loop cycle
+        // Store an object to prevent the result being falsy
+        immediateResult = {result: result}
+        return result
+      })
+      .catch(function (error) {
+        // Fetch error if it is available in the same event-loop cycle
+        // Store an object to prevent the result being falsy
+        immediateError = {error: error}
+      })
+
+    if (immediateResult) {
+      return immediateResult.result
+    }
+    if (immediateError) {
+      throw immediateError.error
+    }
+
+    promises.push(result)
+    result.toString = function () {
+      return options.placeholder + (promises.length - 1) + '>'
+    }
+    return result
   }
 
   return engine
+}
+
+/**
+ * Wrap a function with a wrapper function
+ * @param {function} fn the original function
+ * @param {function(function,array)} wrapper the wrapper-function
+ *   receiving `fn` as first parameter and the arguments as second
+ * @returns {function} a function that calls the wrapper with
+ *   the original function and the arguments
+ */
+function wrap (fn, wrapper) {
+  return function () {
+    var args = Array.prototype.slice.call(arguments)
+    return wrapper.call(this, fn, args)
+  }
 }
