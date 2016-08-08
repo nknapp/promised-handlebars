109bdbf53b9579952c20b5788a195bfc896dfaef
wangyu <wy@terminus.io>
2016-08-08 16:16:13 +0800

    fix bug

--- index.js
+++ index.js
@@ -218,6 +218,9 @@ Markers.prototype.resolve = function resolve (input) {
          * @returns {string}
          */
         function replacePlaceholdersRecursivelyIn (string) {
+          if(typeof string !== 'string'){
+            return string;
+          }
           return string.replace(self.regex, function (match, index, gt) {
             // Check whether promise result must be escaped
             var resolvedValue = promiseResults[index]
