1b487e4ef9179477ed17d8ad9f6d1491505595a8
Nils Knappmeier <npm@knappi.org>
2016-08-27 00:24:10 +0200

    Disable failing test (until the purpose of the test is cleared)
    The purpose of the test is not clear (see #22). The test depends on the
    current version of `bluebird`. When a new version is published, the test will fail (like it does now).
    
    Until #22 is resolved, the test should be disabled.

--- test/global-bluebird-promise-spec.js
+++ test/global-bluebird-promise-spec.js
@@ -8,9 +8,9 @@
 /* global after */
 /* global before */
 /* global describe */
-/* global it */
+// /* global it */
 // /* global xdescribe */
-// /* global xit */
+/* global xit */
 
 'use strict'
 
@@ -31,7 +31,7 @@ describe('promised-handlebars:', function () {
     delete this.Handlebars
   })
   describe('running with ' + promiseName, function () {
-    it('Handlebars.Promise.version should match bluebird package\'s semver', function () {
+    xit('Handlebars.Promise.version should match bluebird package\'s semver', function () {
       // var bluebirdVersion = require('bluebird/package').version
       // The version string provided by require('bluebird').version is out of date in the
       // latest version of the module. For now the test needs to hardcode the value.
