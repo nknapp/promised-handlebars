0d992dbfd95c94c11c181166e62b7962534fb185
Nils Knappmeier <npm@knappi.org>
2016-09-02 15:46:51 +0200

    Make tests run with Mocha 3.0
    Mocha 3 does not allow a Promise as return value **and** a "done"-callback being called.
    see  https://github.com/mochajs/mocha/issues/2407

--- test/default-suite.js
+++ test/default-suite.js
@@ -26,53 +26,46 @@ function runDefaultSuite () {
   })
 
   // Default test suite
-  it('should return a promise for the ouput with helpers resolved', function (done) {
+  it('should return a promise for the ouput with helpers resolved', function () {
     var template = this.Handlebars.compile(fixture('simple-helper.hbs'))
     return expect(template({a: 'abc', b: 'xyz'}))
       .to.eventually.equal('123 h(abc) 456 h(xyz)')
-      .notify(done)
   })
 
-  it('should handle null and undefined arguments of helpers', function (done) {
+  it('should handle null and undefined arguments of helpers', function () {
     var template = this.Handlebars.compile(fixture('simple-helper.hbs'))
     return expect(template({a: null, b: undefined}))
       .to.eventually.equal('123 h(null) 456 h(undefined)')
-      .notify(done)
   })
 
-  it('should work with block helpers that call `fn` while resolving a promise', function (done) {
+  it('should work with block helpers that call `fn` while resolving a promise', function () {
     var template = this.Handlebars.compile(fixture('block-helper.hbs'))
     return expect(template({a: 'abc', b: 'xyz'}))
       .to.eventually.equal('123 b(abc) 456\n123 bi(cba) 456')
-      .notify(done)
   })
 
-  it('should work with a helper being called from within a block helpers', function (done) {
+  it('should work with a helper being called from within a block helpers', function () {
     var template = this.Handlebars.compile(fixture('nested-helpers.hbs'))
     return expect(template({a: 'abc', b: 'xyz'}))
       .to.eventually.equal('block b( h(abc) ) 45\ninverse bi( h(cba) ) 456')
-      .notify(done)
   })
 
-  it('should handle {{expr}} and {{{expr}}} like Handlebars does', function (done) {
+  it('should handle {{expr}} and {{{expr}}} like Handlebars does', function () {
     var template = this.Handlebars.compile(fixture('escaping.hbs'))
     return expect(template({a: '<a>', b: '<b>'}))
       .to.eventually.equal('raw: <a> h(<a>)\nesc: &lt;a&gt; h(&lt;a&gt;)')
-      .notify(done)
   })
 
-  it('should work correctly when partials are called', function (done) {
+  it('should work correctly when partials are called', function () {
     var template = this.Handlebars.compile(fixture('partials.hbs'))
     return expect(template({a: 'aa', b: 'bb'}))
       .to.eventually.equal('h(partialA) 123 h(aa) h(partialB) 456 h(bb)')
-      .notify(done)
   })
 
-  it('the options.fn()-method should not return a promise for synchronous block helpers', function (done) {
+  it('the options.fn()-method should not return a promise for synchronous block helpers', function () {
     var template = this.Handlebars.compile(fixture('synchronous-block-helper.hbs'))
     return expect(template({}))
       .to.eventually.equal('abc,abc')
-      .notify(done)
   })
 
   it('simple helpers should also be able to return real values', function (done) {
@@ -82,58 +75,50 @@ function runDefaultSuite () {
       .notify(done)
   })
 
-  it('helpers passed in as parameters like {{#helper (otherhelper 123)}} should be resolved within the helper call', function (done) {
+  it('helpers passed in as parameters like {{#helper (otherhelper 123)}} should be resolved within the helper call', function () {
     var template = this.Handlebars.compile(fixture('helper-as-parameter.hbs'))
     return expect(template({}))
       .to.eventually.equal('index.js (1)\nondex.js (0)')
-      .notify(done)
   })
 
-  it('helpers passed into partials as parameters like {{>partial (helper 123)}} should be resolved within the helper call', function (done) {
+  it('helpers passed into partials as parameters like {{>partial (helper 123)}} should be resolved within the helper call', function () {
     var template = this.Handlebars.compile(fixture('helper-as-parameter-for-partial.hbs'))
     return expect(template({}))
       .to.eventually.equal('id(h(abc))')
-      .notify(done)
   })
 
-  it('helpers passed in via as hash-parameter like {{#helper param=(otherhelper 123)}} should be resolved within the helper call', function (done) {
+  it('helpers passed in via as hash-parameter like {{#helper param=(otherhelper 123)}} should be resolved within the helper call', function () {
     var template = this.Handlebars.compile(fixture('helper-as-hash.hbs'))
     return expect(template({}))
       .to.eventually.equal('hash(false=h(false),true=h(true))')
-      .notify(done)
   })
-  it('async helpers nested in synchronous block-helpers should work', function (done) {
+  it('async helpers nested in synchronous block-helpers should work', function () {
     var template = this.Handlebars.compile(fixture('synchronous-block-helper-nests-async.hbs'))
     return expect(template({a: 'aa'}))
       .to.eventually.equal('h(aa),h(aa)')
-      .notify(done)
   })
-  it('async helpers nested in synchronous builtin block-helpers should work', function (done) {
+  it('async helpers nested in synchronous builtin block-helpers should work', function () {
     var template = this.Handlebars.compile(fixture('builtin-block-helper-nests-async.hbs'))
     return expect(template({arr: [{a: 'aa'}, {a: 'bb'}]}))
       .to.eventually.equal('h(aa)-h(bb)-')
-      .notify(done)
   })
 
-  it('a completely synchronous block helper (with synchronous resolvable content) not have to deal with placeholders', function (done) {
+  it('a completely synchronous block helper (with synchronous resolvable content) not have to deal with placeholders', function () {
     var template = this.Handlebars.compile(fixture('block-helper-manipulate.hbs'))
     return expect(template({arr: [{a: 'aa'}, {a: 'bb'}]}))
       .to.eventually.equal('abc')
-      .notify(done)
   })
 
-  it('should handle block helpers returning promises with Handlebars.SafeString correctly', function (done) {
+  it('should handle block helpers returning promises with Handlebars.SafeString correctly', function () {
     var template = this.Handlebars.compile('abc{{#safeStringBlock}}<abc>{{/safeStringBlock}}')
     return expect(template({}))
       .to.eventually.equal('abc<abc>')
-      .notify(done)
   })
 
-  it('should handle helpers returning promises with Handlebars.SafeString correctly', function (done) {
+  it('should handle helpers returning promises with Handlebars.SafeString correctly', function () {
     var template = this.Handlebars.compile('abc{{safeString}}')
     return expect(template({}))
       .to.eventually.equal('abc<abc>')
-      .notify(done)
   })
 }
 
